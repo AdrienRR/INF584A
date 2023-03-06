86562b629e576986d65014b9430a9a8fba67d24a
AdrienRR <69118801+AdrienRR@users.noreply.github.com>
2023-03-06 18:03:36 +0100

    Overlord circles around

--- BT/BT_STARCRAFT/BT_ACTION_ZIGZAG_OVERLORD.cpp
+++ BT/BT_STARCRAFT/BT_ACTION_ZIGZAG_OVERLORD.cpp
@@ -24,26 +24,42 @@ BT_NODE::State BT_ACTION_ZIGZAG_OVERLORD::MoveOverlord(void* data)
 
     BWAPI::Position overlordPosition = overlord->getPosition();
 
-    BWAPI::Position myBasePosition = BWAPI::Position(BWAPI::Broodwar->self()->getStartLocation().makeValid());
+    BWAPI::Position base2overlord = overlordPosition - pData->enemyBase;
 
-    BWAPI::Position targetPosition = overlord->getTargetPosition();
+    BWAPI::Position perpendicular = BWAPI::Position(-base2overlord.y, base2overlord.x);
 
+    BWAPI::Position targetPosition = overlordPosition + perpendicular;
 
-    if (targetPosition == myBasePosition)
-    {
-        if (Tools::distance(overlordPosition, pData->enemyBase) > 750) {
-            overlord->move(pData->enemyBase);
-            std::string message = "Zigzaging towards enemy base: " + std::to_string(targetPosition.x) + ", " + std::to_string(targetPosition.y);
-            BWAPI::Broodwar->sendText(message.c_str());
-        }
+    if (Tools::distance(overlordPosition, pData->enemyBase) > 200) {
+        targetPosition = pData->enemyBase;
     }
-    else
-    {
-        if (Tools::distance(overlordPosition, pData->enemyBase) < 250) {
-            overlord->move(myBasePosition);
-            std::string message = "Zigzaging towards own base: " + std::to_string(targetPosition.x) + ", " + std::to_string(targetPosition.y);
-            BWAPI::Broodwar->sendText(message.c_str());
-        }
+    else if (Tools::distance(overlordPosition, pData->enemyBase) < 100) {
+        targetPosition += base2overlord;
     }
+
+    overlord->move(targetPosition);
+    std::string message = "Circling";
+    BWAPI::Broodwar->sendText(message.c_str());
+
+
+
+
+
+    //if (targetPosition == myBasePosition)
+    //{
+    //    if (Tools::distance(overlordPosition, pData->enemyBase) > 750) {
+    //        overlord->move(pData->enemyBase);
+    //        std::string message = "Zigzaging towards enemy base: " + std::to_string(targetPosition.x) + ", " + std::to_string(targetPosition.y);
+    //        BWAPI::Broodwar->sendText(message.c_str());
+    //    }
+    //}
+    //else
+    //{
+    //    if (Tools::distance(overlordPosition, pData->enemyBase) < 250) {
+    //        overlord->move(myBasePosition);
+    //        std::string message = "Zigzaging towards own base: " + std::to_string(targetPosition.x) + ", " + std::to_string(targetPosition.y);
+    //        BWAPI::Broodwar->sendText(message.c_str());
+    //    }
+    //}
     return BT_NODE::SUCCESS;
 }
